9ee4ddc97ccdd40806aa46d13b96bddf7a19fd29
Eustacia Sukarto <eustaciasukarto@gmail.com>
2022-11-19 21:02:35 -0800

    finish explore + add screenshots + readme

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Lab 8 - Starter
 
-1) Where would you fit your automated tests in your Recipe project development pipeline?
+Name: Eustacia Sukarto
 
-Answer: 1. Within a Github action that runs whenever code is pushed: I want to to test my code everytime I push changes to my repo to ensure that that new code does not break the existing code. Github Actions will help me automate my testing process so I do not have to run the tests manually everytime I push changes to my repo.
+1) 1. Within a Github action that runs whenever code is pushed: I want to to test my code everytime I push changes to my repo to ensure that that new code does not break the existing code. Github Actions will help me automate my testing process so I do not have to run the tests manually everytime I push changes to my repo.
 
-2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+2) No.
 
-Answer: No.
+3) No. Unit testing is used to test smaller parts of code. The "message" feature described would contain multiple components interacting with one another, but unit tests does not allow me to test these interactions. Instead, I would use multiple unit tests to test each individual function of the feature.
+
+4) Yes. The "max message length" feature is small enough to be tested with unit testing. Its individual functionality can be tested using unit tests because it does not require interaction with other components since it only checks if the input is more than 80 characters.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Tests for isPhoneNumber()
+test('(123) 456-7890 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('123-456-7890 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('123-a56-7890 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('123-a56-7890')).toBe(false);
+});
+
+test('1234567890 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+// Tests for isEmail()
+test('abc@ucsd.edu is a valid email', () => {
+    expect(functions.isEmail('abc@ucsd.edu')).toBe(true);
+});
+
+test('ABC@UCSD.EDU is a valid email', () => {
+    expect(functions.isEmail('ABC@UCSD.EDU')).toBe(true);
+});
+
+test('^-^@ucsd.edu is not a valid email', () => {
+    expect(functions.isEmail('^-^@ucsd.edu')).toBe(false);
+});
+
+test('abc@ucsd is not a valid email', () => {
+    expect(functions.isEmail('abc@ucsd')).toBe(false);
+});
+
+// Tests for isStrongPassword()
+test('abc123_456 is a strong password', () => {
+    expect(functions.isStrongPassword('abc123_456')).toBe(true);
+});
+
+test('A1b2C3d4EFGH is a strong password', () => {
+    expect(functions.isStrongPassword('A1b2C3d4EFGH')).toBe(true);
+});
+
+test('1abcDEF_23 is not a strong password', () => {
+    expect(functions.isStrongPassword('1abcDEF_23')).toBe(false);
+});
+
+test('abc is not a strong password', () => {
+    expect(functions.isStrongPassword('abc')).toBe(false);
+});
+
+// Tests for isDate()
+test('1/1/2022 is a date', () => {
+    expect(functions.isDate('1/1/2022')).toBe(true);
+});
+
+test('01/01/2022 is a date', () => {
+    expect(functions.isDate('01/01/2022')).toBe(true);
+});
+
+test('/1/2022 is not a date', () => {
+    expect(functions.isDate('/1/2022')).toBe(false);
+});
+
+test('01-01-2022 is not a date', () => {
+    expect(functions.isDate('01-01-2022')).toBe(false);
+});
+
+// Tests for isHexColor()
+test('#AAA000 is a hex color', () => {
+    expect(functions.isHexColor('#AAA000')).toBe(true);
+});
+
+test('AaA is a hex color', () => {
+    expect(functions.isHexColor('AaA')).toBe(true);
+});
+
+test('QAQ is not a hex color', () => {
+    expect(functions.isHexColor('QAQ')).toBe(false);
+});
+
+test('#AAA!AA is not a hex color', () => {
+    expect(functions.isHexColor('#AAA!AA')).toBe(false);
+});
